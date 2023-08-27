4fd27b98e7cbe82665a228def295ce203bca2952
Greg <greg.pounds3@gmail.com>
2023-08-27 01:22:40 -0500

    add refactor

--- MortgageCalc/src/App.java
+++ MortgageCalc/src/App.java
@@ -2,43 +2,52 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class App {
+    final static byte MONTHS_IN_YEAR =12;
+        final static byte PERCENT = 100;
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         NumberFormat currency = NumberFormat.getCurrencyInstance();
-        int principal = 0;
-        float annualRate = 0;
-        int years = 0;
-        int months =0;
-        float monthlyRate=0;
-        double mortgage = 0;
-
-        while (true){
-            System.out.print("Principal ($1K-$1M): ");
-            principal = scanner.nextInt();
-            if (principal >= 1_000 && principal <= 1_000_000)
-                break;
-            System.out.println("Enter a number between 1,000 and 1,000,000.");
-        }
-        while (true){
-            System.out.print("Annual Interest Rate: ");
-            annualRate = scanner.nextFloat();
-            if (annualRate > 0 && annualRate <= 30){
-                monthlyRate = annualRate / 100 / 12;
-                break;
-            }
-            System.out.println("Enter a value greater than 0 and less than or equal to 30.");
-        }
-        while (true){
-            System.out.print("Period (Years): ");
-            years = scanner.nextInt();
-            if (years > 0 && years <= 30){
-                months = years * 12;
-                break;
-            }
-            System.out.println("Enter a value greater than 0 and less than or equal to 30.");
-        }
-        mortgage = (principal * ((monthlyRate * Math.pow(monthlyRate + 1, months)) / (Math.pow(monthlyRate + 1, months) - 1)));
+        int principal = (int) readNumber("Principle: ", 1000, 1_000_000);
+        float annualRate = (float) readNumber("Annual Interest Rate: ", 1, 30);
+        byte years = (byte) readNumber("Period (Years): ", 1, 30);
+        double mortgage = calculateMortgage(principal, annualRate, years);
         final String result = currency.format(mortgage);
-        System.out.println("\nYour Mortgage: " + result);
+        System.out.println();
+        System.out.println("MORTGAGE");
+        System.out.println("--------");
+        System.out.println("Monthly Payments: " + result);
+        
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("----------------");
+        for (short month = 1; month <= years * MONTHS_IN_YEAR; month++){
+            double balance = calculateBalance(principal, annualRate, years, month);
+            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+        }
+    }
+    public static double readNumber(String prompt, double min, double max){
+        Scanner scanner = new Scanner(System.in);
+        double value;
+        while (true) {
+            System.out.print(prompt);
+            value = scanner.nextFloat();
+            if (value >= min && value <= max) break;
+            System.out.println("Enter a value between " + min + " and " + max);
+        }
+        return value;
+    }
+    public static double calculateBalance(int principal, float annualRate, byte years, short numberOfPaymentsMade){
+        float monthlyRate = annualRate / PERCENT / MONTHS_IN_YEAR;
+        float numberOfPayments = years * MONTHS_IN_YEAR;
+        double balance = principal * (Math.pow(1 + monthlyRate, numberOfPayments)- Math.pow(1 + monthlyRate, numberOfPaymentsMade)) / (Math.pow(1 + monthlyRate, numberOfPayments) - 1);
+        return balance;
+    }
+    public static double calculateMortgage(int principal, float annualRate, byte years) {
+        double result;
+        float monthlyRate;
+        float numberOfPayments = years * MONTHS_IN_YEAR;
+        monthlyRate = annualRate / PERCENT / MONTHS_IN_YEAR;
+        result = principal
+                * (monthlyRate * Math.pow(monthlyRate + 1, numberOfPayments)) / (Math.pow(monthlyRate + 1, numberOfPayments) - 1);
+        return result;
     }
 }
--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Name: ");
+        String name = scanner.nextLine().trim();
+        System.out.println("You are " + name);
     }
 }
